d83ac7a1eae539899018c93a6c39b003661d26c3
dominhquan <dominhquan.uit@gmail.com>
2014-09-21 13:34:12 +0700

    Initial commit

---
+++ DataStructure/Debug/src/DataStructure.d
@@ -0,0 +1,1 @@
+src/DataStructure.d: ../src/DataStructure.cpp
---
+++ DataStructure/src/DataStructure.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DataStructure.cpp
+// Author      : Do Minh Quan -- 11520616
+// Version     :
+// Copyright   : Data Structure & Algorithms
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
