fc0b4221b300ec6a258c89f71aa674a54699d2e6
dominhquan <dominhquan.uit@gmail.com>
2014-12-21 20:07:38 +0700

    update

--- DataStructure/Final.cpp
+++ DataStructure/Final.cpp
@@ -2,7 +2,21 @@
 #include <string>
 #include <string.h>
 #include <sstream>
+#include <ctime>
 using namespace std;
+
+typedef struct TS{
+	std::string hoten;
+	std::string ngaysinh;
+	std::string noisinh;
+	std::string coquan;
+	int diem;
+}TS;
+
+typedef struct NodeTS{
+	TS ts;
+	NodeTS *pNext;
+}NodeTS;
 
 typedef struct SV{
 	std::string name;
@@ -17,10 +31,13 @@ typedef struct Node{
 }Node;
 
 typedef struct List{
-	Node *pHead;
-	Node *pTail;
+	NodeTS *pHead;
+	NodeTS *pTail;
 }List;
 
+typedef struct Tree{
+
+}Tree;
 class Stack{
 	private:
 		int size;
@@ -101,14 +118,11 @@ class Queue{
 			else{
 				if (size == 1){
 					front->pNext = tmp;
-					rear = tmp;
-					rear->pNext = NULL;
-				}
-				else{
+				}else{
 					rear->pNext = tmp; // set current rear->nextNode =  temp Node
-					rear = tmp;        // Now rear=temp Node
-					rear->pNext = NULL;
 				}
+				rear = tmp;
+				rear->pNext = NULL;
 			}
 			size++;
 		}
@@ -158,70 +172,172 @@ class Queue{
 			return size == 0 ? true : false;
 		}
 };
+
 class LinkedList{
-	void createList(List &list){
-		list.pHead = NULL;
-		list.pTail = NULL;
-	}
-	Node* createNode(SV sv){
-		Node *tmp = new Node;
-		if (tmp == NULL) exit(1);
-		tmp->sv = sv;
-		tmp->pNext = NULL;
-		return tmp;
-	}
-	void addHead(List &list, Node* node){
-		if (list.pHead == NULL){
-			list.pHead = node;
-			list.pTail = list.pHead;
+	private:
+		int size;
+		List *list;
+	public:
+		LinkedList(){
+			list = new List;
+			list->pHead = NULL;
+			list->pTail = NULL;
+			size = 0;
 		}
-		else{
-			node->pNext = list.pHead;
-			list.pHead = node;
+		
+		void addHead(TS ts){
+			NodeTS *node = new NodeTS;
+			node->ts = ts;
+			if (isEmpty()){
+				node->pNext = NULL;
+				list->pHead = node;
+				list->pTail = node;
+			}
+			else{
+				node->pNext = list->pHead;
+				list->pHead = node;
+			}
+			size++;
 		}
-	}
+
+		void addTail(TS ts){
+			NodeTS *node = new NodeTS;
+			node->ts = ts;
+			if (isEmpty()){
+				node->pNext = NULL;
+				list->pHead = node;
+				list->pTail = node;
+			}
+			else{
+				if (size == 1){
+					list->pHead->pNext = node;
+				}
+				else{
+					list->pTail->pNext = node; // set current rear->nextNode =  temp Node
+				}
+				list->pTail = node;
+				list->pTail->pNext = nullptr;
+			}
+			size++;
+		}
+
+		TS searchList(std::string hoten){
+			if (isEmpty()) exit(1);
+			NodeTS *tmp = list->pHead;
+			while (tmp->ts.hoten != hoten){
+				tmp = tmp->pNext;
+			}
+			return tmp->ts;
+		}
+
+		void selectionSort(){
+			NodeTS *nextNode, *minNode;
+			NodeTS *currentHead = list->pHead;
+			while (currentHead != list->pTail){
+				minNode = currentHead;
+				nextNode = currentHead->pNext;
+				while (nextNode){
+					if (nextNode->ts.diem < currentHead->ts.diem){
+						minNode = nextNode;
+					}
+					nextNode = nextNode->pNext;
+				}
+				TS tmp = minNode->ts;
+				minNode->ts = currentHead->ts;
+				currentHead->ts = tmp;
+				currentHead = currentHead->pNext;
+			}
+		}
+
+		void quickSortList(List &list){
+			NodeTS *p, *x;
+			List l1, l2;
+			if (list.pHead = list.pTail) return;
+
+		}
+		void printList(){
+			if (isEmpty()){
+				cout << "List is empty" << endl;
+			}
+			else{
+				NodeTS *currentFront = list->pHead;
+				cout << "--- TS Info ---" << endl;
+				while (currentFront){
+					cout << "Name :" << currentFront->ts.hoten << "  ---- Diem : "<<currentFront->ts.diem<<endl;
+					currentFront = currentFront->pNext;
+				}
+				cout << "--- End TS Info ---" << endl;
+			}
+		}
+
+
+		NodeTS* getNode(List &list,std::string hoten){
+			if (!isEmpty()){
+				NodeTS *tmp = list.pHead;
+				while (tmp){
+					if (tmp->ts.hoten == hoten){
+						return tmp;
+					}
+					tmp = tmp->pNext;
+				}
+				size--;
+				return tmp;
+			}
+			exit(1);
+		}
+
+		int deleteNode(List &list, TS ts){
+			if (!isEmpty()){
+				NodeTS *p=list.pHead;
+				NodeTS *q = nullptr;
+				while (p && (p->ts.hoten!=ts.hoten)){
+					q = p;
+					p = p->pNext;
+				}
+				if (p == NULL){ return 0; }
+				if (q != NULL){}
+				else{ removeHead(list, ts); }
+				size--;
+				return 1;
+			}
+			return -1;
+		}
+		
+		int removeHead(List &list, TS ts){
+			if (isEmpty()){ return 0; }
+			NodeTS *node=list.pHead;
+			node->ts = ts;
+			list.pHead = list.pHead->pNext;
+			delete node;
+			if (isEmpty()){
+				list.pHead = nullptr;
+				list.pTail = nullptr;
+			}
+			return 1;
+		}
+
+		bool isEmpty(){
+			return size == 0 ? true : false;
+		}
+
+};
+
+class BinaryTree{
+	
+};
+
+class SearchAndSort{
+	
 };
 
 int main(){
 	SV *sv = new SV;
-
-	/* -----------------------------------  /
-	/  ------------Linked list------------  /
-	/  ----------------------------------- */
-
-
-	/* -----------------------------------  / 
-	/  ---------------Stack---------------  / 
-	/  ----------------------------------- */
+	TS *ts = new TS;
+	srand(time(NULL));
+	LinkedList *list = new LinkedList;
 	Stack *stack = new Stack;
-	for(int i = 1; i < 4; i++){
-		std::stringstream out;
-		out << i;
-		sv->name = "SV." + out.str();
-		sv->khoa = i;
-		sv->makhoa = "MMT.0" + out.str();
-		sv->mssv = 1152000 + i;
-		stack->push(*sv);
-	}
-	cout << "-----------------Before POP----------------" << endl;
-	stack->printStack();
-	*sv = stack->pop(); 
-	cout << "Pop :" << sv->name << endl; 
-	cout << "-----------------After POP----------------" << endl;
-	stack->printStack();
-	*sv = stack->pop(); 
-	cout << "Pop :" << sv->name << endl;
-	cout << "-----------------After POP----------------" << endl;
-	stack->printStack();
-	*sv = stack->pop();
-	cout << "Pop :" << sv->name << endl;
-	cout << "-----------------After POP----------------" << endl;
-	stack->printStack();
-
-	/* -----------------------------------  /
-	/  ---------------Queue---------------  /
-	/  ----------------------------------- */
 	Queue *queue = new Queue;
+
 	for (int i = 1; i < 4; i++){
 		std::stringstream out;
 		out << i;
@@ -229,27 +345,73 @@ int main(){
 		sv->khoa = i;
 		sv->makhoa = "MMT.0" + out.str();
 		sv->mssv = 1152000 + i;
+		stack->push(*sv);
 		queue->enQueue(*sv);
 	}
-	cout << "-----------------Before Dequeue----------------" << endl;
-	queue->printQueue(); 
-    *sv=queue->deQueue();
-	cout << "Dequeue :" << sv->name << endl;
-	cout << "-----------------After Dequeue----------------" << endl;
-	queue->printQueue();
-	*sv = queue->deQueue();
-	cout << "Dequeue :" << sv->name << endl;
-	cout << "-----------------After Dequeue----------------" << endl;
-	queue->printQueue();
-	*sv = queue->deQueue();
-	cout << "Dequeue :" << sv->name << endl;
-	cout << "-----------------After Dequeue----------------" << endl;
-	queue->printQueue();
+
+	/* -----------------------------------  /
+	/  ------------Linked list------------  /
+	/  ----------------------------------- */
+	for (int i = 1; i < 21; i++){
+		std::stringstream out;
+		out << i;
+		ts->hoten = "TS.0" + out.str();
+		ts->coquan = "UIT";
+		ts->noisinh = "HCM City";
+		ts->diem = rand() % 10 + 1;
+		ts->ngaysinh = "1/1/1993";
+		list->addHead(*ts);
+	}
+	cout << "-----------------Before Sort----------------" << endl;
+	list->printList();
+	list->selectionSort();
+	cout << "-----------------After Sort----------------" << endl;
+	list->printList();
+//
+//	/* -----------------------------------  / 
+//	/  ---------------Stack---------------  / 
+//	/  ----------------------------------- */
+//	cout << "-----------------Before POP----------------" << endl;
+//	stack->printStack();
+//	*sv = stack->pop(); 
+//	cout << "Pop :" << sv->name << endl; 
+//	cout << "-----------------After POP----------------" << endl;
+//	stack->printStack();
+//	*sv = stack->pop(); 
+//	cout << "Pop :" << sv->name << endl;
+//	cout << "-----------------After POP----------------" << endl;
+//	stack->printStack();
+//	*sv = stack->pop();
+//	cout << "Pop :" << sv->name << endl;
+//	cout << "-----------------After POP----------------" << endl;
+//	stack->printStack();
+//
+//	/* -----------------------------------  /
+//	/  ---------------Queue---------------  /
+//	/  ----------------------------------- */
+//	cout << "-----------------Before Dequeue----------------" << endl;
+//	queue->printQueue(); 
+//    *sv=queue->deQueue();
+//	cout << "Dequeue :" << sv->name << endl;
+//	cout << "-----------------After Dequeue----------------" << endl;
+//	queue->printQueue();
+//	*sv = queue->deQueue();
+//	cout << "Dequeue :" << sv->name << endl;
+//	cout << "-----------------After Dequeue----------------" << endl;
+//	queue->printQueue();
+//	*sv = queue->deQueue();
+//	cout << "Dequeue :" << sv->name << endl;
+//	cout << "-----------------After Dequeue----------------" << endl;
+//	queue->printQueue();
 
 
 
 	/* -----------------------------------  /
 	/  --------Binary Search Tree---------  /
 	/  ----------------------------------- */
+
+
+
+
 	return 0;
 }
